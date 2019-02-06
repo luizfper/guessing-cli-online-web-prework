23e21d0b59d7f78cfe120f3e40890d12fe2451d1
Luiz Pereira <luizfper@gmail.com>
2019-02-06 00:50:10 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     answer = gets.chomp
     if answer == "exit"
-      puts "Goodbye!"
+      exit
       break
     elsif answer.to_i.between?(1,6)
       comp = rand(1..6)
@@ -18,4 +18,7 @@ def run_guessing_game
   end
 end
 
+def exit
+ puts "Goodbye!"
+end
 #run_guessing_game
