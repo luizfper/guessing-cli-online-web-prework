7565696c697ca71c8b61aac0ddbed3ff3119289e
Luiz Pereira <luizfper@gmail.com>
2019-02-06 00:43:57 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,7 +12,7 @@ def run_guessing_game
       if answer.to_i ==comp
         puts "You guessed the correct number!"
       else
-        puts "The computer guessed #{comp-1}"
+        puts "The computer guessed #{comp}"
       end
     end
   end
