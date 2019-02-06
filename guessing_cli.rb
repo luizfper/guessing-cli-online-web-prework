b3ef17ab907833d078885859fed22a69b2d61cdf
Luiz Pereira <luizfper@gmail.com>
2019-02-06 00:22:47 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   loop do
     comp = 0
-    print "Guess a number between 1 and 6."
+    puts "Guess a number between 1 and 6."
     answer = gets.chomp
     case answer
     when "exit"
